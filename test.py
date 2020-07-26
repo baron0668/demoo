dbefa531798e5946f6d916a1ecf1527efba66a28
Anoymous Hentai <60800427+Anonymous-Hentai@users.noreply.github.com>
2020-07-26 16:51:05 +0800

    Support windows, upgrade to Python 3

--- test.py
+++ test.py
@@ -5,14 +5,22 @@ import sys
 from os.path import expanduser
 import sqlite3
 import json
-from StringIO import StringIO
+from io import StringIO
 from Crypto.PublicKey import RSA
 
-homePath = expanduser("~")
-booksPath = "books"
+homePath = expanduser('~')
+booksPath = 'books'
 downloadPath = 'download'
-deDrmOutputPath = "deDrmBooks"
-dbPath = homePath + "/Library/Application Support/Readmoo/Local Storage/" + "app_readmoo_0.localstorage"
+deDrmOutputPath = 'deDrmBooks'
+
+if sys.platform == 'darwin':
+	dbPath = homePath + '/Library/Application Support/Readmoo/Local Storage/' + 'app_readmoo_0.localstorage'
+elif sys.platform == 'win32':
+	dbPath =  homePath + '/AppData/Local/Readmoo/Local Storage/app_readmoo_0.localstorage'
+else:
+	print('not support for ', sys.platform, '. return.')
+	sys.exit()
+
 epubPathDownload = None
 rsaPrivateKey = None
 userId = None
@@ -27,28 +35,28 @@ def getEpubUrl(bookId):
 	return epubDownloadUrl + bookId + '?client_id='+ clientId + '&access_token=' +userToken
 
 def downloadEpub(bookId):
-	import urllib2
+	import urllib.request, urllib.error, urllib.parse
 
 	url = getEpubUrl(bookId)
 	downloadToPath = os.path.join(downloadPath, userId)
 	downloadToFileName = bookId + '.epub'
 	outputFile = os.path.join(downloadToPath, downloadToFileName)
 	if os.path.exists(outputFile):
-		print outputFile, ' is exists. return.'
+		print(outputFile, ' is exists. return.')
 		return
 
-	print 'open link: ', url
-	response = urllib2.urlopen(url)
+	print('open link: ', url)
+	response = urllib.request.urlopen(url)
 
 	#check folder
 	if not os.path.exists(downloadToPath):
 		os.makedirs(downloadToPath)
 	
-	print 'create file: ', outputFile
+	print('create file: ', outputFile)
 	file = open(outputFile, 'wb')
-	print 'downloading...'
+	print('downloading...')
 	file.write(response.read())
-	print 'downloading finished.'
+	print('downloading finished.')
 	
 
 def extractEpub(bookId):
@@ -59,7 +67,7 @@ def extractEpub(bookId):
 	if not os.path.exists(toPath):
 		os.makedirs(toPath)
 	zf = zipfile.ZipFile(epubFile, mode='r')
-	print 'extract epub file...'
+	print('extract epub file...')
 	zf.extractall(toPath)
 	zf.close()
 
@@ -83,48 +91,48 @@ def decryptFiles(aesKey, bookId, encryptedFiles, outputRootPath):
 	from Crypto.Cipher import AES
 
 	rootPath = os.path.join(epubPathDownload, bookId)
-	outputBookPath = outputRootPath + "/" + bookId
-	print 'aes key ', aesKey.encode('hex')
-	print 'input path ', rootPath
-	print 'out path ', outputBookPath
+	outputBookPath = outputRootPath + '/' + bookId
+	print('aes key ', str(aesKey))
+	print('input path ', rootPath)
+	print('out path ', outputBookPath)
 
 	for file in encryptedFiles:
-		filePath = rootPath + "/" + file
-		outputFile = outputBookPath + "/" + file
+		filePath = rootPath + '/' + file
+		outputFile = outputBookPath + '/' + file
 		fileName = os.path.basename(filePath)
 		dirName = os.path.dirname(outputFile)
 		if not os.path.exists(dirName):
 			os.makedirs(dirName)
 
-		f = open(filePath, 'r')
+		f = open(filePath, 'rb')
 		inputBytes = f.read()
 		iv = inputBytes[:16]
 		cipherText = inputBytes[16:]
 		aesCipher = AES.new(aesKey, AES.MODE_CBC, iv)
-		print "decrypting file: ", fileName
+		print('decrypting file: ', fileName)
 		decryptBytes = aesCipher.decrypt(cipherText)
 		decryptBytesLen = len(decryptBytes)
 		#remove padding
 		lastByte = decryptBytes[decryptBytesLen-1: decryptBytesLen]
 		paddingLength = 0
-		print 'last 16 byte: ', decryptBytes[decryptBytesLen-16: decryptBytesLen].encode('hex')
-		# print 'last byte: ', lastByte.encode('hex')
+		print('last 16 byte: ', str(decryptBytes[decryptBytesLen-16: decryptBytesLen]))
+		# print('last byte: ', lastByte.encode('hex'))
 		for index in range(decryptBytesLen-1, decryptBytesLen-17, -1):
 			if decryptBytes[index] == lastByte:
 				paddingLength += 1
 			else:
 				break;
-		print "padding: ", lastByte.encode('hex'), "  padding length: ", paddingLength
+		print('padding: ', str(lastByte), '  padding length: ', paddingLength)
 
 
-		print 'add new file: ', outputFile
-		f = open(outputFile, 'w')
+		print('add new file: ', outputFile)
+		f = open(outputFile, 'wb')
 		f.write(decryptBytes[:decryptBytesLen-paddingLength])
-		print "decrypt ", file, " is finished."
+		print('decrypt ' + file + ' is finished.')
 
 def checkOtherFiles(bookId, outputRootPath):
 	rootPath = os.path.join(epubPathDownload, bookId)
-	outPath = outputRootPath + "/" + bookId
+	outPath = outputRootPath + '/' + bookId
 
 	for r, d, f in os.walk(rootPath):
 		for name in d:
@@ -133,7 +141,7 @@ def checkOtherFiles(bookId, outputRootPath):
 			outputDir = os.path.join(outPath, outputDir)
 			if not os.path.exists(outputDir):
 				#create folder
-				print "create folder: ", outputDir
+				print('create folder: ', outputDir)
 				os.makedirs(outputDir)
 		for name in f:
 			basename = os.path.basename(name)
@@ -144,27 +152,27 @@ def checkOtherFiles(bookId, outputRootPath):
 				if not os.path.exists(outputFile):
 					#copy file
 					srcFile = os.path.join(r, name)
-					file = open(srcFile, 'r')
+					file = open(srcFile, 'rb')
 					inputData = file.read()
-					print "create file: ", outputFile
-					file = open(outputFile, 'w')
+					print('create file: ', outputFile)
+					file = open(outputFile, 'wb')
 					file.write(inputData)
 
 def outputDecryptedEpubFile(bookId, outputPath):
 	import zipfile
-	outputZipFilePath = outputPath + "/" + bookId + ".epub"
-	booksPath = outputPath + "/" + bookId
-	print 'create zip file: ', outputZipFilePath 
+	outputZipFilePath = outputPath + '/' + bookId + '.epub'
+	booksPath = outputPath + '/' + bookId
+	print('create zip file: ', outputZipFilePath)
 	zf = zipfile.ZipFile(outputZipFilePath, mode='w')
 	for r, d, f in os.walk(booksPath):
 		for name in f:
 			filePath = os.path.join(r, name)
 			zipFilePath = os.path.relpath(filePath, booksPath)
-			file = open(filePath, 'r')
+			file = open(filePath, 'rb')
 			fileByte = file.read()
 			zf.writestr(zipFilePath, fileByte)
 
-	print zf.printdir()
+	print(zf.printdir())
 	zf.close()
 
 def decryptBook(bookId):
@@ -188,28 +196,23 @@ def decryptBook(bookId):
 	#decrypt aes key
 	aesKey = decryptAESKey(rsaPrivateKey, mainEncryptedKey)
 
-	decryptFiles(aesKey, bookId, encryptedFilesReference, "output")
-	print "checking other files..."
-	checkOtherFiles(bookId, "output")
-	outputDecryptedEpubFile(bookId, "output")
-	
-#check platform
-if sys.platform != 'darwin':
-	print 'not support for ', sys.platform, ". return."
-	sys.exit()
+	decryptFiles(aesKey, bookId, encryptedFilesReference, 'output')
+	print('checking other files...')
+	checkOtherFiles(bookId, 'output')
+	outputDecryptedEpubFile(bookId, 'output')
 
 #read db from readmoo
 conn = sqlite3.connect(dbPath)
 cursor = conn.execute('SELECT * FROM ItemTable')
 for row in cursor:
 	if row[0] == 'rsa_privateKey':
-		rsaPrivateKey = str(row[1]).decode('utf16')
+		rsaPrivateKey = row[1].decode('utf16')
 	elif row[0] == '-nw-library':
-		userBooksLibraryData = str(row[1]).decode('utf16')
+		userBooksLibraryData = row[1].decode('utf16')
 	elif row[0] == '-nw-access_token':
-		userToken = str(row[1]).decode('utf16')
+		userToken = row[1].decode('utf16')
 	elif row[0] == '-nw-userid':
-		userId = str(row[1]).decode('utf16')
+		userId = row[1].decode('utf16')
 conn.close()
 
 epubPathDownload = os.path.join(downloadPath, userId)
@@ -222,22 +225,23 @@ for item in userBooksLibraryData:
 	title = item['library_item']['book']['title']
 	currentBooksInLibrary.update({bookId: title})
 
-allBooksId = currentBooksInLibrary.keys()
+allBooksId = list(currentBooksInLibrary.keys())
 if len(allBooksId) <= 0:
-	print 'there is no book to encrypt. finish.'
+	print('there is no book to encrypt. finish.')
 	sys.exit()
 else:
-	print 'user id: ', userId
-	print 'all your books:'
-	#print books list
+	print('user id: ', userId)
+	print('all your books:')
+	#print(books list
 	for index in range(0, len(allBooksId)):
 		bookId = allBooksId[index]
-		print '    #', index, currentBooksInLibrary[bookId], '(', bookId, ')' 
-	print ''
+		print('    #', index, currentBooksInLibrary[bookId], '(', bookId, ')' )
+	print('')
 
 	inputNumber = -1
+	
 	while inputNumber < 0 or inputNumber >= len(allBooksId):
-		inputNumber = input('input the number to decrypt: ')
+		inputNumber = int(eval(input('input the number to decrypt: ')))
 	#process the book
 	choosedBookId = allBooksId[inputNumber]
 
